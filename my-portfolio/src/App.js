98fcb35fddc31ea16909898152180d5cd49598c1
bsester <124089310+bsester@users.noreply.github.com>
2024-05-11 20:48:32 -0500

    image size css

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -11,23 +11,23 @@ function App() {
           'to create a companion app for displaying research about their products. The application also supports journaling and allowing ' +
           'users to record and rate which products helped them the most. The app was developed using React Native, Firebase and Firestore. ' +
           'Note that the repository is private. Please contact me to see the code.',
-      imageUrl: 'https://via.placeholder.com/150',
+      imageUrl: 'https://cdn.worldvectorlogo.com/logos/react-native-1.svg',
       githubUrl: 'https://github.com/FringeCapstone/client'
     },
     {
       title: 'Multithreaded Image Processor',
-      description: 'We developed a program in c to process images using Message Passing Interface (MPI).' +
+      description: 'We developed a program in c++ to process images using Message Passing Interface (MPI).' +
           ' The multiprocessing implementation was up to 1600% faster than the single-threaded implementation.' +
           ' We studied the speedup and efficiency across different numbers of threads and different image sizes, ' +
           ' on operations such as changing the image to grayscale or sorting the pixels.',
-      imageUrl: 'https://via.placeholder.com/150',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png',
       githubUrl: 'https://github.com/bsester/Multithreaded-Image-Processor'
     },
     {
       title: 'Schedulemaker',
       description: 'My teammates and I created an app to streamline the creation of academic schedules for Illinois State University.' +
             ' My role in the project was the aggregation of course information through a webscraper, and parsing that information.',
-      imageUrl: 'https://via.placeholder.com/150',
+      imageUrl: 'https://www.svgrepo.com/show/303388/java-4-logo.svg',
       githubUrl: 'https://github.com/FisherZE/ScheduleMaker'
       },
 
@@ -44,7 +44,7 @@ function App() {
             {projects.map((project, index) => (
                 <div key={index} className="project">
                   <h2>{project.title}</h2>
-                  <img src={project.imageUrl} alt={project.title} />
+                  <img className={"projectImg"} src={project.imageUrl} alt={project.title} />
                   <p>{project.description}</p>
                   <div className="project-links">
                     <a href={project.githubUrl} target="_blank" rel="noopener noreferrer">GitHub</a>
