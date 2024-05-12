78cc27572e505036a1c7636ccdd8a278c028b07f
bsester <124089310+bsester@users.noreply.github.com>
2024-05-11 20:43:35 -0500

    filled in projects

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -6,25 +6,29 @@ function App() {
   // Sample projects data
   const projects = [
     {
-      title: 'Project 1',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta mauris id massa commodo, sit amet convallis ex sollicitudin.',
+      title: 'Project Fringe',
+      description: 'I worked with in collaboration with my teammates and Fringe, a company specializing in alternative medicine, ' +
+          'to create a companion app for displaying research about their products. The application also supports journaling and allowing ' +
+          'users to record and rate which products helped them the most. The app was developed using React Native, Firebase and Firestore. ' +
+          'Note that the repository is private. Please contact me to see the code.',
       imageUrl: 'https://via.placeholder.com/150',
-      liveUrl: 'https://example.com/project1',
-      githubUrl: 'https://github.com/yourusername/project1'
+      githubUrl: 'https://github.com/FringeCapstone/client'
     },
     {
-      title: 'Project 2',
-      description: 'Praesent laoreet feugiat orci, vitae lobortis sem vehicula in. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam ut magna a mi volutpat bibendum.',
+      title: 'Multithreaded Image Processor',
+      description: 'We developed a program in c to process images using Message Passing Interface (MPI).' +
+          ' The multiprocessing implementation was up to 1600% faster than the single-threaded implementation.' +
+          ' We studied the speedup and efficiency across different numbers of threads and different image sizes, ' +
+          ' on operations such as changing the image to grayscale or sorting the pixels.',
       imageUrl: 'https://via.placeholder.com/150',
-      liveUrl: 'https://example.com/project2',
-      githubUrl: 'https://github.com/yourusername/project2'
+      githubUrl: 'https://github.com/bsester/Multithreaded-Image-Processor'
     },
     {
-      title: 'Project 2',
-      description: 'Praesent laoreet feugiat orci, vitae lobortis sem vehicula in. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam ut magna a mi volutpat bibendum.',
+      title: 'Schedulemaker',
+      description: 'My teammates and I created an app to streamline the creation of academic schedules for Illinois State University.' +
+            ' My role in the project was the aggregation of course information through a webscraper, and parsing that information.',
       imageUrl: 'https://via.placeholder.com/150',
-      liveUrl: 'https://example.com/project2',
-      githubUrl: 'https://github.com/yourusername/project2'
+      githubUrl: 'https://github.com/FisherZE/ScheduleMaker'
       },
 
     // Add more projects as needed
